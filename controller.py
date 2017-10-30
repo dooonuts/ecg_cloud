16f65d43f7ca79d05e37b7ec4fe5b816e26e4474
VCM admin account <vcm@vcm-1915.vm.duke.edu>
2017-10-29 23:42:38 -0400

    Not sure what I have committed, but it's something

---
+++ controller.py
@@ -0,0 +1,18 @@
+from bme590hrm import hrm_class as hrm_class
+import pandas
+
+def main():
+   return 1;
+
+def summary(data_dict):
+   
+   return 1;
+
+def data_format(data_dict):
+   return 1;
+
+def init(data_dict):
+   return 1;
+
+if __name__ == '__main__':
+    main()
