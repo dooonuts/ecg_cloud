88495694ac2ff8afd3ac6e203ef67535f3d47921
VCM admin account <vcm@vcm-1915.vm.duke.edu>
2017-10-31 03:17:24 -0400

    Looking what the csv file looks like

--- controller.py
+++ controller.py
@@ -13,10 +13,12 @@ def summary(data_dict):
 def data_format(data_dict):
    print(data_dict['times'])
    print(data_dict['voltages'])
-   with open('hrdata.csv', 'w') as f:  # Just use 'w' mode in 3.x
-    w = csv.DictWriter(f, data_dict.keys())
-    w.writeheader()
-    w.writerow(data_dict)
+   #with open('hrdata.csv', 'w') as f:  # Just use 'w' mode in 3.x
+   # w = csv.DictWriter(f, data_dict.keys())
+   # w.writeheader()
+   # w.writerow(row)
+   #f.close()
+   pandas.DataFrame(data_dict,index=[0]).to_csv('hrdata.csv')
    return 1
 
 def average(data_dict):
