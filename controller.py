425a7a86b6f0627671774124a50a4ca872ee82cd
VCM admin account <vcm@vcm-1915.vm.duke.edu>
2017-10-31 15:46:06 -0400

    Need to fix parts of bme590hrm

--- controller.py
+++ controller.py
@@ -3,41 +3,35 @@ import pandas
 import csv
 
 def main():
-   return 1;
+    return 1
+
 
 def summary(data_dict):
-   form = data_format(data_dict)
-      
-   return 1
+    data_format(data_dict)
+    hrm_object = init()
+    # need to add instant
+    tachy = hrm_object.anomaly_tf.tachy_tf
+    brady = hrm_object.anomaly_tf.brady_tf
+    return [tachy,brady]
+
 
 def data_format(data_dict):
-   list(t) for t in zip(*data_dict)
-   print(data_dict2)
-   times  = data_dict['times']
-   voltages = data_dict['voltages']
-   print(type(times))
-   print(times)
-   #with open('hrdata.csv', 'w') as f:  # Just use 'w' mode in 3.x
-   # w = csv.DictWriter(f, data_dict.keys())
-   # w.writeheader()
-   # w.writerow(row)
-   #f.close()
-   #rows = zip(data_dict['times'],data_dict['voltages'])
-   #with open('hrdata.csv', "w") as f:
-   # writer = csv.writer(f,delimiter= '')
-   # for row in rows:
-   #     writer.writerow(row)
-   with open('hrdata.csv', 'wb') as f:
-    writer = csv.writer(f)
-    writer.writerows(zip(*data_dict)) 
- 
-   return 1
+    times = data_dict['time']
+    voltages = data_dict['voltage']
+    rows = zip(times, voltages)
+    with open('hrdata.csv', "w") as f:
+        writer = csv.writer(f, delimiter=' ')
+        for row in rows:
+            writer.writerow(row)
+
 
 def average(data_dict):
-   return 1
+    return 1
 
-def init(data_dict):
-   return 1
+
+def init():
+    hrm_object = hrm_class.HrmData('hrdata.csv')
+    return hrm_object
 
 if __name__ == '__main__':
     main()
