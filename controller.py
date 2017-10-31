a2542b3ea7b7cffe0f8ddc55e6b087c45b96d600
VCM admin account <vcm@vcm-1915.vm.duke.edu>
2017-10-31 15:10:14 -0400

    Removed pycache

--- controller.py
+++ controller.py
@@ -11,18 +11,26 @@ def summary(data_dict):
    return 1
 
 def data_format(data_dict):
-   print(data_dict['times'])
-   print(data_dict['voltages'])
+   list(t) for t in zip(*data_dict)
+   print(data_dict2)
+   times  = data_dict['times']
+   voltages = data_dict['voltages']
+   print(type(times))
+   print(times)
    #with open('hrdata.csv', 'w') as f:  # Just use 'w' mode in 3.x
    # w = csv.DictWriter(f, data_dict.keys())
    # w.writeheader()
    # w.writerow(row)
    #f.close()
-   rows = zip(data_dict['times'],data_dict['voltages'])
-   with open('hrdata.csv', "w") as f:
+   #rows = zip(data_dict['times'],data_dict['voltages'])
+   #with open('hrdata.csv', "w") as f:
+   # writer = csv.writer(f,delimiter= '')
+   # for row in rows:
+   #     writer.writerow(row)
+   with open('hrdata.csv', 'wb') as f:
     writer = csv.writer(f)
-    for row in rows:
-        writer.writerow([row])
+    writer.writerows(zip(*data_dict)) 
+ 
    return 1
 
 def average(data_dict):
