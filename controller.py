b57b2367a55e296e65e6270090926db55eb7a47f
iruvdonuts <dw172@duke.edu>
2017-10-31 19:53:37 -0400

    Made pep8 changes

--- controller.py
+++ controller.py
@@ -2,6 +2,7 @@ from bme590hrm import hrm_class as hrm_class
 import pandas
 import csv
 
+
 def main():
     return 1
 
@@ -10,9 +11,9 @@ def summary(data_dict):
     data_format(data_dict)
     hrm_object = init()
     # need to add instant
-    instant = hrm_object.instantaneous_hr 
-    tachy   = hrm_object.anomaly_tf.tachy_tf
-    brady   = hrm_object.anomaly_tf.brady_tf
+    instant = hrm_object.instantaneous_hr
+    tachy = hrm_object.anomaly_tf.tachy_tf
+    brady = hrm_object.anomaly_tf.brady_tf
     return [data_dict['time'], instant, tachy, brady]
 
 
@@ -30,16 +31,16 @@ def average(data_dict):
     data_format(data_dict)
     hrm_object = init()
     average = hrm_object.average_hr
-    tachy   = hrm_object.anomaly_tf.tachy_tf
-    brady   = hrm_object.anomaly_tf.brady_tf
-    return [data_dict['averaging_period'], data_dict['time'], \
-      average, tachy, brady]
-
+    tachy = hrm_object.anomaly_tf.tachy_tf
+    brady = hrm_object.anomaly_tf.brady_tf
+    return [data_dict['averaging_period'], data_dict['time'],
+            average, tachy, brady]
 
 
 def init():
     hrm_object = hrm_class.HrmData('hrdata.csv')
     return hrm_object
+
 
 if __name__ == '__main__':
     main()
--- routes.py
+++ routes.py
@@ -2,58 +2,66 @@ import sys
 import json
 import controller
 from flask import Flask, request, render_template
- 
-app = Flask(__name__)      
- 
+
+app = Flask(__name__)
+
+
 @app.route('/')
 def home():
-  return render_template('home.html')
+    return render_template('home.html')
+
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
 
-@app.route('/data',methods = ['POST'])
+
+@app.route('/data', methods=['POST'])
 def postJSONHandler():
-  print("Here")
-  if(request.is_json):
-    content=request.get_json()
-    print(content)
-  return 'content'
+    print("Here")
+    if(request.is_json):
+        content = request.get_json()
+        print(content)
+    return 'content'
 
-@app.route('/api/heart_rate/summary', methods= ['POST'])
+
+@app.route('/api/heart_rate/summary', methods=['POST'])
 def instantaneous():
-  if(request.is_json):
-    data_dict = request.get_json()
-    print(data_dict)
-    # [time, instant_hr, tachy, brady] =  controller.summary(data_dict)
-    [tachy, brady] = controller.summary(data_dict)
-    # jsonify the lists
-    #return_dict = { "time" : time, "instantaneous_heart_rate" : instant_hr, \
-    #  "tachycardia_annotations" : tachy, "bradycardia_annotations" : brady }
-    return_dict = {"tachycardia_annotations" : tachy, "bradycardia_annotations" : brady }
-    json_info = json.dumps(return_dic)
-    return json_info
-  else:
-    raise ValueError("Did not input a JSON file") 
+    if(request.is_json):
+        data_dict = request.get_json()
+        print(data_dict)
+        # [time, instant_hr, tachy, brady] =  controller.summary(data_dict)
+        [tachy, brady] = controller.summary(data_dict)
+        # jsonify the lists
+        # return_dict = { "time" : time, "instantaneous_heart_rate" : instant_hr, \
+        #  "tachycardia_annotations" : tachy, "bradycardia_annotations" : brady }
+        return_dict = {
+            "tachycardia_annotations": tachy,
+            "bradycardia_annotations": brady}
+        json_info = json.dumps(return_dic)
+        return json_info
+    else:
+        raise ValueError("Did not input a JSON file")
 
-@app.route('/api/heart_rate/average', methods= ['POST'])
+
+@app.route('/api/heart_rate/average', methods=['POST'])
 def average():
-  if(request.is_json):
-    data_dict = request.get_json()
-    [avg_per, time_int, average_hr, tachy, brady] = controller.average(data_dict)
-    # jsonify the lists 
-    return_dict = { "averaging_period" : avg_per, "time_interval" : time_int, \
-      "average_heart_rate" : average_hr, "tachycardia_annotations" : tachy, \
-      "bradycardia_annotations" : brady }
-    json_info = json.dumps(return_dic)
-    return json_info
-  else:
-    raise ValueError("Did not input a JSON file") 
+    if(request.is_json):
+        data_dict = request.get_json()
+        [avg_per, time_int, average_hr, tachy,
+            brady] = controller.average(data_dict)
+        # jsonify the lists
+        return_dict = {
+            "averaging_period": avg_per,
+            "time_interval": time_int,
+            "average_heart_rate": average_hr,
+            "tachycardia_annotations": tachy,
+            "bradycardia_annotations": brady}
+        json_info = json.dumps(return_dic)
+        return json_info
+    else:
+        raise ValueError("Did not input a JSON file")
 
 
-
-    
- 
 if __name__ == '__main__':
-  app.run(debug=True, host= '0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
