9adbeccfba3147f4eb5653db260f6f50830c9854
Daniel Wu <dw352934@gmail.com>
2017-11-01 21:10:56 -0400

    Changed controller.py to work on stuff

--- controller.py
+++ controller.py
@@ -7,8 +7,18 @@ def main():
     return 1
 
 
+def average(data_dict):
+     averaging_period = data_format_average(data_dict)
+     hrm_object = init()
+     average = hrm_object.average_hr
+     tachy = hrm_object.anomaly_tf.tachy_tf
+     brady = hrm_object.anomaly_tf.brady_tf
+     return [data_dict['averaging_period'], data_dict['time'],
+         average, tachy, brady]
+
+
 def summary(data_dict):
-    data_format(data_dict)
+    data_format_summary(data_dict)
     hrm_object = init()
     # need to add instant
     instant = hrm_object.instantaneous_hr
@@ -17,7 +27,7 @@ def summary(data_dict):
     return [data_dict['time'], instant, tachy, brady]
 
 
-def data_format(data_dict):
+def data_format_summary(data_dict):
     times = data_dict['time']
     voltages = data_dict['voltage']
     rows = zip(times, voltages)
@@ -27,14 +37,16 @@ def data_format(data_dict):
             writer.writerow(row)
 
 
-def average(data_dict):
-    data_format(data_dict)
-    hrm_object = init()
-    average = hrm_object.average_hr
-    tachy = hrm_object.anomaly_tf.tachy_tf
-    brady = hrm_object.anomaly_tf.brady_tf
-    return [data_dict['averaging_period'], data_dict['time'],
-            average, tachy, brady]
+def data_format_average(data_dict):
+    times = data_dict['time']
+    voltages = data_dict['voltage']
+    averaging_period = data_dict['averaging_period']
+    rows = zip(times, voltages)
+    with open('hrdata.csv', "w") as f:
+         writer = csv.writer(f, delimiter=',')
+         for row in rows:
+             writer.writerow(row)
+    return averaging_period
 
 
 def init():
